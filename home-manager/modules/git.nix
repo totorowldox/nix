acfadff59572ab8f9d2ebc99ded0cb680bebc053
totorowldox <totorowldox@gmail.com>
2025-09-10 22:56:55 +0800

    fix: git ssh

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -7,8 +7,8 @@
 
       gpg."ssh".program = "${pkgs._1password-gui}/bin/op-ssh-sign";
 
-      http.proxy = "127.0.0.1:7890";
-      https.proxy = "127.0.0.1:7890";
+      http.proxy = "http://127.0.0.1:7890";
+      https.proxy = "http://127.0.0.1:7890";
 
       commit = { gpgsign = true; };
 
--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,13 +1,16 @@
 _:
 let
-  # onePassPath = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
   onePassPath = "~/.1password/agent.sock";
 in {
   programs.ssh = {
     enable = true;
     extraConfig = ''
-      Host *
-          IdentityAgent ${onePassPath}
+    Host *
+      IdentityAgent ${onePassPath}
+    Host github.com
+      Hostname ssh.github.com
+      Port 443
+      User git
     '';
   };
 }
