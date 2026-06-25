580a642ef580eec1a6ebef9a275dc8deb01f9971
totorowldox <totorowldox@gmail.com>
2026-06-25 12:44:04 +0800

    add: plymouth

--- modules/common/bootloader.nix
+++ modules/common/bootloader.nix
@@ -2,7 +2,19 @@
   # Choose one bootloader.
   boot.loader.grub.enable = true;
   boot.loader.systemd-boot.enable = false;
+  boot.plymouth.enable = true;
+  boot.initrd.systemd.enable = true;
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
 
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+  ];
   boot.loader.efi = {
     canTouchEfiVariables = true;
     efiSysMountPoint = "/boot";
